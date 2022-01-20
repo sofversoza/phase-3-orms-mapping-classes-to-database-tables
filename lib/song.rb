92300ecfc7f6e59b5c8f4e7c5877f3971c60e8fa
sofiaversoza <sofversoza@gmail.com>
2022-01-19 18:46:54 -0700

    commit tree 44d271861e1d3364b69ecdc173216eff35b9cd6f

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,46 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+
+  def save
+    # creating the table
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+   
+    # insert the song
+    DB[:conn].execute(sql, self.name, self.album)
+  
+    # get the song ID from the database (column of the last inserted row) & save it to the Ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  
+    # return the Ruby instance/obj
+    self
+  end
+
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+  end
+  
 end
